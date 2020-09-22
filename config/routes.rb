c64d4175ef1bcba67d621d6ef33754ef23065fec
naokikubo2 <naoki16871@gmail.com>
2020-09-22 12:01:34 +0900

    add users controller #7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/new'
+  get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get    '/',        to: 'homes#top'
