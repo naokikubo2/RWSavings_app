6527d592595946bdada3bb6440be5fdb52629cb5
naokikubo2 <naoki16871@gmail.com>
2020-09-22 11:32:49 +0900

    add logout func #16

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get    '/',        to: 'homes#top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  delete '/logout',  to: 'sessions#destroy'
 end
