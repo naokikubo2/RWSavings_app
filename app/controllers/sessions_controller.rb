0c87877c632d06edba9cc6a762f6323ca84d796e
naokikubo2 <naoki16871@gmail.com>
2020-09-22 18:06:17 +0900

    add flash_message #34

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,17 @@ class SessionsController < ApplicationController
     if @user&.authenticate(params[:session][:password])
       log_in(@user)
       @current_user = @user
+      flash[:success] = 'ログインに成功しました'
       redirect_to '/'
     else
+      flash[:warning] = 'ログインに失敗しました'
       redirect_to '/login'
     end
   end
 
   def destroy
     log_out if logged_in?
+    flash[:success] = 'ログアウトしました'
     redirect_to '/login'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = 'ユーザ登録に成功しました'
       redirect_to '/'
     else
+      flash[:warning] = 'ユーザ登録に失敗しました'
       render 'new'
     end
   end
