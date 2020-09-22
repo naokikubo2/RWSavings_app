8cbbd976f7eff5932c72fd4fc9e21027a979b816
naokikubo2 <naoki16871@gmail.com>
2020-09-22 13:03:48 +0900

    add route users_controller #32

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   get    '/',        to: 'homes#top'
   delete '/logout',  to: 'sessions#destroy'
+  resources :users
 end
